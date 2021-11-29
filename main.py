7c2a062dc02015b09707a6dbdd25f200cf4318e6
Shalika22 <67798098+Shalika22@users.noreply.github.com>
2021-11-29 22:58:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from tkinter import *
 from PIL import ImageTk,Image
 import pymysql
 from tkinter import messagebox
-from AddBook import *
-from DeleteBook import *
-from ViewBooks import *
-from IssueBook import *
-from ReturnBook import *
+from studentLogin import *
+from adminLogin import *
+
 # Add your own database name and password here to reflect in the code
-mypass = "root"
+
+mypass = "sumanth@123"
 mydatabase="db"
 
 con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
@@ -48,19 +47,10 @@ headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
 headingLabel = Label(headingFrame1, text="Welcome to \n DataFlair Library", bg='black', fg='white', font=('Courier',15))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
-btn1 = Button(root,text="Add Book Details",bg='black', fg='white', command=addBook)
-btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
-    
-btn2 = Button(root,text="Delete Book",bg='black', fg='white', command=delete)
-btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
-    
-btn3 = Button(root,text="View Book List",bg='black', fg='white', command=View)
-btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
-    
-btn4 = Button(root,text="Issue Book to Student",bg='black', fg='white', command = issueBook)
-btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
-    
-btn5 = Button(root,text="Return Book",bg='black', fg='white', command = returnBook)
-btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
+btn1 = Button(root,text="Student",bg='black', fg='white', command = student)
+btn1.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
+
+btn2 = Button(root,text="Admin",bg='black', fg='white', command = admin)
+btn2.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
 
 root.mainloop()
